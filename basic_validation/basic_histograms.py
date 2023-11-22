c97cf682aa0d26de281ad4efaf6cee3079d0d47c
j-s-ashley <jashley6@vols.utk.edu>
2023-11-22 23:41:12 +0000

    Basic histogram bin updates

--- basic_validation/basic_histograms.py
+++ basic_validation/basic_histograms.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Change run number
-hepmc_file = "../../run_data/run_15/Events/run_01/tag_1_pythia8_events.hepmc"
+hepmc_file = "../../run_data/run_17/Events/run_01/tag_1_pythia8_events.hepmc"
 
 pos_particle_pts = []
 pos_particle_etas = []
@@ -22,8 +22,8 @@ with pyhepmc.open(hepmc_file) as f:
     for i, event in enumerate(f):
         for particle in event.particles:
             if abs(particle.pid) == 2000013:
-                num_particles += 1
                 if particle.pid == 2000013 and particle.status == 22:
+                    num_particles += 1
                     p_pt = mev_to_gev( particle.momentum.pt() )
                     pos_particle_pts.append(p_pt)
                     p_eta = particle.momentum.eta()
@@ -31,6 +31,7 @@ with pyhepmc.open(hepmc_file) as f:
                     p_phi = particle.momentum.phi()
                     pos_particle_phis.append(p_phi)
                 elif particle.pid == -2000013 and particle.status == 22:
+                    num_particles += 1
                     n_pt = mev_to_gev( particle.momentum.pt() )
                     neg_particle_pts.append(n_pt)
                     n_eta = particle.momentum.eta()
@@ -42,17 +43,18 @@ with pyhepmc.open(hepmc_file) as f:
 tags = ['Positively charged smuons', 'Negatively charged smuons']
 
 # --- pT ---
-pT_num_bins = 20
 pTs = [pos_particle_pts, neg_particle_pts]
 
-plt.hist(pTs, bins=pT_num_bins, histtype='bar', label=tags, stacked=True)
-#plt.hist(neg_particle_pts, bins=pT_num_bins, histtype='step', label='Negatively charged smuons', stacked=True)
+pT_bin_width = 125
+pT_num_bins = np.linspace(np.min(pTs), np.max(pTs) + pT_bin_width, pT_bin_width)
+
+plt.hist(pTs, bins=pT_num_bins, histtype='bar', label=tags, stacked=True, edgecolor='black')
 
 plt.xlabel('pT [GeV]')
 plt.ylabel('Number of Smuons')
-plt.legend()
 plt.title('Smuon pT')
 
+plt.legend()
 plt.grid(color='c')
 
 plt.savefig('Smuon_pT.pdf', bbox_inches='tight')
@@ -61,14 +63,22 @@ plt.clf()
 
 # --- eta ---
 etas = [pos_particle_etas, neg_particle_etas]
-plt.hist(etas, histtype='bar', label=tags, stacked=True)
-#plt.hist(neg_particle_etas, histtype='step', label='Negatively charged smuons', stacked=True)
+# weirdly won't work; tried abs and np.abs; trying something more convoluted for ss&gs
+eta_extrema = max( np.abs( np.min(etas) ), np.max(etas) ) 
+# eta_min = 0 - np.min(etas)
+# eta_max = np.max(etas)
+# eta_extrema = np.max( eta_min, eta_max)
+
+eta_bin_width = 16
+eta_num_bins = np.linspace(-eta_extrema, eta_extrema + eta_bin_width, eta_bin_width)
+
+plt.hist(etas, bins=eta_num_bins, histtype='bar', label=tags, stacked=True, edgecolor='black')
 
 plt.xlabel('$\eta$')
 plt.ylabel('Number of Smuons')
-plt.legend()
 plt.title('Smuon $\eta$')
 
+plt.legend()
 plt.grid(color='c')
 
 plt.savefig('Smuon_eta.pdf', bbox_inches='tight')
@@ -76,19 +86,25 @@ plt.savefig('Smuon_eta.png', bbox_inches='tight')
 plt.clf()
 
 # --- phi ---
-phi_range = ( -(np.pi), np.pi )
 phis = [pos_particle_phis, neg_particle_phis]
 
-plt.hist(phis, range=phi_range, histtype='bar', label=tags, stacked=True)
-#plt.hist(neg_particle_phis, range=phi_range, histtype='step', label='Negatively charged smuons', stacked=True)
+phi_bin_width = 1 
+phi_num_bins = np.linspace( -(np.pi), np.pi + phi_bin_width, phi_bin_width )
+
+plt.hist(phis, bins=phi_num_bins, histtype='bar', label=tags, stacked=True, edgecolor='black')
 
 plt.xlabel('$\phi$')
 plt.ylabel('Number of Smuons')
-plt.legend()
 plt.title('Smuon $\phi$')
 
+plt.legend()
 plt.grid(color='c')
 
 plt.savefig('Smuon_phi.pdf', bbox_inches='tight')
 plt.savefig('Smuon_phi.png', bbox_inches='tight')
 
+# --- misc ---
+print(num_particles,'smuons generated')
+print(np.max(pTs),'max pT')
+print(eta_extrema,'max eta')
+print(np.sum(phis) / num_particles,'average pT')
--- basic_validation/pltmv.sh
+++ basic_validation/pltmv.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir run_15v2
-mv Smuon_eta.pdf run_15v2
-mv Smuon_eta.png run_15v2
-mv Smuon_phi.pdf run_15v2
-mv Smuon_phi.png run_15v2
-mv Smuon_pT.pdf run_15v2
-mv Smuon_pT.png run_15v2
-mv run_15v2 plots
+mkdir run_17
+mv Smuon_eta.pdf run_17
+mv Smuon_eta.png run_17
+mv Smuon_phi.pdf run_17
+mv Smuon_phi.png run_17
+mv Smuon_pT.pdf run_17
+mv Smuon_pT.png run_17
+mv run_17 plots
