faae3ffbb7a64417fa0970f3bd2cb44efcb7632c
j-s-ashley <jashley6@vols.utk.edu>
2023-12-02 16:00:39 +0000

    Attempted to filter muons by parents. Vertex tracing hard. 'Continue'ing for momentary sanity.

--- basic_validation/muon_plotter.py
+++ basic_validation/muon_plotter.py
@@ -22,22 +22,27 @@ with pyhepmc.open(hepmc_file) as f:
     for i, event in enumerate(f):
         for particle in event.particles:
             if abs(particle.pid) == 13:
-                if particle.pid == 13 and particle.status == 1:
-                    num_particles += 1
-                    p_pt = mev_to_gev( particle.momentum.pt() )
-                    pos_particle_pts.append(p_pt)
-                    p_eta = particle.momentum.eta()
-                    pos_particle_etas.append(p_eta)
-                    p_phi = particle.momentum.phi()
-                    pos_particle_phis.append(p_phi)
-                elif particle.pid == -13 and particle.status == 1:
-                    num_particles += 1
-                    n_pt = mev_to_gev( particle.momentum.pt() )
-                    neg_particle_pts.append(n_pt)
-                    n_eta = particle.momentum.eta()
-                    neg_particle_etas.append(n_eta)
-                    n_phi = particle.momentum.phi()
-                    neg_particle_phis.append(n_phi)
+                if particle.status == 1:
+                    for p in particle.production_vertex.particles_in:
+                        if abs(p.pid) == 2000013:
+                            if particle.pid == 13 and particle.status == 1:
+                                num_particles += 1
+                                p_pt = mev_to_gev( particle.momentum.pt() )
+                                pos_particle_pts.append(p_pt)
+                                p_eta = particle.momentum.eta()
+                                pos_particle_etas.append(p_eta)
+                                p_phi = particle.momentum.phi()
+                                pos_particle_phis.append(p_phi)
+                            elif particle.pid == -13 and particle.status == 1:
+                                num_particles += 1
+                                n_pt = mev_to_gev( particle.momentum.pt() )
+                                neg_particle_pts.append(n_pt)
+                                n_eta = particle.momentum.eta()
+                                neg_particle_etas.append(n_eta)
+                                n_phi = particle.momentum.phi()
+                                neg_particle_phis.append(n_phi)
+                        elif abs(p.pid) == 13:
+                            continue
 
 # HISTOGRAM STUFF
 tags = ['Positively charged muons', 'Negatively charged muons']
--- basic_validation/pltmv2.sh
+++ basic_validation/pltmv2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir run_17muons
-mv muon_eta.png run_17muons
-mv muon_phi.png run_17muons
-mv muon_pT.png run_17muons
-mv run_17muons plots
+mkdir run_17muons_v2
+mv muon_eta.png run_17muons_v2
+mv muon_phi.png run_17muons_v2
+mv muon_pT.png run_17muons_v2
+mv run_17muons_v2 plots
