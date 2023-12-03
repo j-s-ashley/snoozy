55b9b095e2025241b782245b9b8649e38c8f4cef
j-s-ashley <jashley6@vols.utk.edu>
2023-12-03 13:56:37 +0000

    Signal muon tracing problem almost fixed.

--- basic_validation/muon_plotter.py
+++ basic_validation/muon_plotter.py
@@ -16,33 +16,49 @@ neg_particle_phis = []
 def mev_to_gev(mev):				# pT values are in MeV,
 	return mev * 10**-3			    # converting to GeV
 
+def is_signal(parents, family, ancestor):
+    for p in parents:
+        if abs(p.id) == ancestor:
+            return True
+        if abs(p.id) != family:
+            continue
+        parents = p.production_vertex.particles_in
+    return False
+
 num_particles = 0
+
+family = 13 # signal particle id
+ancestor = 2000013 # target parent id
 
 with pyhepmc.open(hepmc_file) as f:
     for i, event in enumerate(f):
+        
+        # Begin looping over event record
         for particle in event.particles:
-            if abs(particle.pid) == 13:
-                if particle.status == 1:
-                    for p in particle.production_vertex.particles_in:
-                        if abs(p.pid) == 2000013:
-                            if particle.pid == 13 and particle.status == 1:
-                                num_particles += 1
-                                p_pt = mev_to_gev( particle.momentum.pt() )
-                                pos_particle_pts.append(p_pt)
-                                p_eta = particle.momentum.eta()
-                                pos_particle_etas.append(p_eta)
-                                p_phi = particle.momentum.phi()
-                                pos_particle_phis.append(p_phi)
-                            elif particle.pid == -13 and particle.status == 1:
-                                num_particles += 1
-                                n_pt = mev_to_gev( particle.momentum.pt() )
-                                neg_particle_pts.append(n_pt)
-                                n_eta = particle.momentum.eta()
-                                neg_particle_etas.append(n_eta)
-                                n_phi = particle.momentum.phi()
-                                neg_particle_phis.append(n_phi)
-                        elif abs(p.pid) == 13:
-                            continue
+
+            # If the particle is a final-state muon, get parents
+            if abs(particle.pid) == family  and particle.status == 1:
+                flag = is_signal(particle.production_vertex.particles_in, family, ancestor)
+
+                # If not signal, move on; else, pull data
+                if flag == False:
+                    continue
+                if particle.pid == family:
+                    num_particles += 1
+                    p_pt = mev_to_gev( particle.momentum.pt() )
+                    pos_particle_pts.append(p_pt)
+                    p_eta = particle.momentum.eta()
+                    pos_particle_etas.append(p_eta)
+                    p_phi = particle.momentum.phi()
+                    pos_particle_phis.append(p_phi)
+                elif particle.pid == -family:
+                    num_particles += 1
+                    n_pt = mev_to_gev( particle.momentum.pt() )
+                    neg_particle_pts.append(n_pt)
+                    n_eta = particle.momentum.eta()
+                    neg_particle_etas.append(n_eta)
+                    n_phi = particle.momentum.phi()
+                    neg_particle_phis.append(n_phi)
 
 # HISTOGRAM STUFF
 tags = ['Positively charged muons', 'Negatively charged muons']
