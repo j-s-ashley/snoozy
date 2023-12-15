9e13e50e5e84ce80b3cd67e1ea4ef3ac24045f99
j-s-ashley <jashley6@vols.utk.edu>
2023-12-15 20:40:11 +0000

    Snagged Alaa's code for recursing signal-finding function. Bonus tidying. Muon plotting functional.

--- basic_validation/muon_plotter.py
+++ basic_validation/muon_plotter.py
@@ -16,14 +16,19 @@ neg_particle_phis = []
 def mev_to_gev(mev):				# pT values are in MeV,
 	return mev * 10**-3			    # converting to GeV
 
-def is_signal(parents, target, ancestor):
-    for p in parents:
-        if abs(p.id) == ancestor:
-            return True
-        if abs(p.id) != target:
-            continue
-        is_signal(p.production_vertex.particles_in, target, ancestor)
-    return False
+def get_final_muon_descendant(particle):
+    # Get the final state muon descendant of a given particle.
+    # Check if the particle itself is a final state muon
+    if particle.status == 1 and abs(particle.pid) == 13:
+        return particle
+    # If the particle has an end vertex, check its descendants recursively
+    elif particle.end_vertex:
+        for p in particle.end_vertex.particles_out:
+            final_muon = get_final_muon_descendant(p)
+            if final_muon is not None:
+                return final_muon
+    # If the particle is not a final state muon and does not have an end vertex, return None
+    return None
 
 num_particles = 0
 
@@ -31,34 +36,31 @@ target = 13 # signal particle id
 ancestor = 2000013 # id of parent of signal particle
 
 with pyhepmc.open(hepmc_file) as f:
-    for i, event in enumerate(f):
-        
-        # Begin looping over event record
+    for event in f:
         for particle in event.particles:
-
-            # If the particle is a final-state muon, get parents
-            if abs(particle.pid) == target  and particle.status == 1:
-                flag = is_signal(particle.production_vertex.particles_in, target, ancestor)
-
-                # If not signal, move on; else, pull data
-                if flag == False:
-                    continue
-                if particle.pid == target:
-                    num_particles += 1
-                    p_pt = mev_to_gev( particle.momentum.pt() )
-                    pos_particle_pts.append(p_pt)
-                    p_eta = particle.momentum.eta()
-                    pos_particle_etas.append(p_eta)
-                    p_phi = particle.momentum.phi()
-                    pos_particle_phis.append(p_phi)
-                elif particle.pid == -target:
-                    num_particles += 1
-                    n_pt = mev_to_gev( particle.momentum.pt() )
-                    neg_particle_pts.append(n_pt)
-                    n_eta = particle.momentum.eta()
-                    neg_particle_etas.append(n_eta)
-                    n_phi = particle.momentum.phi()
-                    neg_particle_phis.append(n_phi)
+            momentum = particle.momentum
+            pt = momentum.pt()
+            # Check if the particle is a muon produced by a decaying 2000013 or -2000013
+            if abs(particle.pid) == 13 and particle.production_vertex and any(p.pid in [2000013, -2000013] for p in particle.production_vertex.particles_in):
+                # Get the final muon descendant of the muon
+                final_muon = get_final_muon_descendant(particle)
+                if final_muon is not None:
+                    if particle.pid == target:
+                        num_particles += 1
+                        p_pt = mev_to_gev( particle.momentum.pt() )
+                        pos_particle_pts.append(p_pt)
+                        p_eta = particle.momentum.eta()
+                        pos_particle_etas.append(p_eta)
+                        p_phi = particle.momentum.phi()
+                        pos_particle_phis.append(p_phi)
+                    elif particle.pid == -target:
+                        num_particles += 1
+                        n_pt = mev_to_gev( particle.momentum.pt() )
+                        neg_particle_pts.append(n_pt)
+                        n_eta = particle.momentum.eta()
+                        neg_particle_etas.append(n_eta)
+                        n_phi = particle.momentum.phi()
+                        neg_particle_phis.append(n_phi)
 
 # HISTOGRAM STUFF
 tags = ['Positively charged muons', 'Negatively charged muons']
@@ -66,8 +68,7 @@ tags = ['Positively charged muons', 'Negatively charged muons']
 # --- pT ---
 pTs = [pos_particle_pts, neg_particle_pts]
 
-pT_bin_width = 125
-pT_num_bins = np.linspace(np.min(pTs), np.max(pTs) + pT_bin_width, pT_bin_width)
+pT_num_bins = np.linspace(0, 3000, 50)
 
 plt.hist(pTs, bins=pT_num_bins, histtype='bar', label=tags, stacked=True, edgecolor='black')
 
@@ -83,9 +84,8 @@ plt.clf()
 
 # --- eta ---
 etas = [pos_particle_etas, neg_particle_etas]
-eta_extrema = max( np.abs( np.min(etas) ), np.max(etas) ) 
-
-eta_num_bins = np.linspace(-eta_extrema, eta_extrema)
+eta_extrema = max (np.abs( np.min(etas) ), np.max(etas) )
+eta_num_bins = np.linspace( -5, 5 )
 
 plt.hist(etas, bins=eta_num_bins, histtype='bar', label=tags, stacked=True, edgecolor='black')
 
--- basic_validation/pltmv2.sh
+++ basic_validation/pltmv2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir run_17muons_v2
-mv muon_eta.png run_17muons_v2
-mv muon_phi.png run_17muons_v2
-mv muon_pT.png run_17muons_v2
-mv run_17muons_v2 plots
+mkdir run_17muons_v3
+mv muon_eta.png run_17muons_v3
+mv muon_phi.png run_17muons_v3
+mv muon_pT.png run_17muons_v3
+mv run_17muons_v3 plots
