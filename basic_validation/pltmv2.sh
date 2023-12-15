93ad364365ffbbaa1676a67e45a59d2b3ae07898
j-s-ashley <jashley6@vols.utk.edu>
2023-12-15 20:54:12 +0000

    Applied working muon_plotter to run_15 data. Filed plots.

--- basic_validation/pltmv2.sh
+++ basic_validation/pltmv2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir run_17muons_v3
-mv muon_eta.png run_17muons_v3
-mv muon_phi.png run_17muons_v3
-mv muon_pT.png run_17muons_v3
-mv run_17muons_v3 plots
+mkdir run_15_muons
+mv muon_eta.png run_15_muons
+mv muon_phi.png run_15_muons
+mv muon_pT.png run_15_muons
+mv run_15_muons plots
