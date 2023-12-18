4affab4f4550bbf1ff767af017d7f15ffd09ee87
j-s-ashley <jashley6@vols.utk.edu>
2023-12-18 17:27:33 +0100

    Confirmed that, once again, .hepmc file is in MeV. Added some detail to proton_p.py for sanity.

--- basic_validation/proton_p.py
+++ basic_validation/proton_p.py
@@ -2,7 +2,7 @@ import pyhepmc
 from particle import literals as lp
 import numpy as np
 
-hepmc_file = "../../run_data/run_15/Events/run_01/tag_1_pythia8_events.hepmc"
+hepmc_file = "../../run_data/run_18/Events/run_01/tag_1_pythia8_events.hepmc"
 
 with pyhepmc.open(hepmc_file) as f:
     for all_events in f:
@@ -15,4 +15,10 @@ def sum_energy_of_protons(event):
     e = p.e
     return np.sum(e[ma])
 
-print( sum_energy_of_protons(all_events) )
+def mev_to_gev(mev):
+    return mev * 10**-3
+
+proton_e = sum_energy_of_protons(all_events)
+print('Unaltered result of total proton energy:', proton_e)
+print( 'After converting from MeV to GeV:', mev_to_gev(proton_e) )
+print('Per run_card.dat, lines 36, 37: "Beam 1, 2 energy = 6500.0 GeV"')
--- basic_validation/unpack.sh
+++ basic_validation/unpack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gzip -d /userdata/jordan/run_data/run_17/Events/run_01/tag_1_pythia8_events.hepmc.gz
-gzip -d /userdata/jordan/run_data/run_17/Events/run_01/unweighted_events.lhe.gz
+gzip -d /eos/home-j/jashley/run_data/run_18/Events/run_01/tag_1_pythia8_events.hepmc.gz
+gzip -d /eos/home-j/jashley/run_data/run_18/Events/run_01/unweighted_events.lhe.gz
