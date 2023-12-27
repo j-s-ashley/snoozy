52439b220f308d6dfe833547a2a1dbc382668994
j-s-ashley <jashley6@vols.utk.edu>
2023-12-27 21:42:40 +0100

    Replaced references to muons with variable input. Bonus modularization.

--- basic_validation/plot_signal_particle_properties.py
+++ basic_validation/plot_signal_particle_properties.py
@@ -2,8 +2,11 @@ import pyhepmc
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Change run number
+# CHANGE ME :)
 hepmc_file = "../../run_data/run_17/Events/run_01/tag_1_pythia8_events.hepmc"
+signal_p_pid= 13                    # signal particle id
+signal_p = 'muon'
+desired_parent = 2000013            # id of parent of signal particle
 
 pos_particle_pts = []
 pos_particle_etas = []
@@ -13,21 +16,25 @@ neg_particle_pts = []
 neg_particle_etas = []
 neg_particle_phis = []
 
-target = 13                         # signal particle id
-desired_parent = 2000013            # id of parent of signal particle
+# Check particle for potential as a signal
+def potential_signal(particle):
+    if abs(particle.pid) == signal_p_pid and particle.production_vertex:
+        if any(p.pid in [desired_parent, -desired_parent] for p in particle.production_vertex.particles_in):
+            return True
+    return False
 
 # Get final state particles
 def get_signal(particle):
     # Check if the particle itself is a signal particle
-    if particle.status == 1 and abs(particle.pid) == target:
+    if particle.status == 1 and abs(particle.pid) == signal_p_pid:
         return particle
     # If the particle has an end vertex, check its descendants recursively
-    elif particle.end_vertex and abs(particle.pid) == target:
+    elif particle.end_vertex and abs(particle.pid) == signal_p_pid:
         for p in particle.end_vertex.particles_out:
             final_particle = get_signal(p)
             if final_particle is not None:
                 return final_particle
-    # If the particle is not a final state muon and does not have an end vertex, return None
+    # If the particle is not a final state particle and does not have an end vertex, return None
     return None
 
 def mev_to_gev(mev):				# pT values are in MeV,
@@ -38,13 +45,12 @@ num_particles = 0
 with pyhepmc.open(hepmc_file) as f:
     for event in f:
         for particle in event.particles:
-            # Check if the particle is a muon produced by a decaying 2000013 or -2000013
-            # TODO This *can* be made a little more tidy. Eventually.
-            if abs(particle.pid) == target and particle.production_vertex and any(p.pid in [desired_parent, -desired_parent] for p in particle.production_vertex.particles_in):
-                # Get the final muon descendant of the muon
+            # Check if the particle is produced by a decaying 2000013 or -2000013
+            if potential_signal(particle):
+            # Get the final descendant of the signal particle
                 signal_particle = get_signal(particle)
                 if signal_particle is not None:
-                    if particle.pid == target:
+                    if particle.pid == signal_p_pid:
                         num_particles += 1
                         p_pt = mev_to_gev( particle.momentum.pt() )
                         pos_particle_pts.append(p_pt)
@@ -52,7 +58,7 @@ with pyhepmc.open(hepmc_file) as f:
                         pos_particle_etas.append(p_eta)
                         p_phi = particle.momentum.phi()
                         pos_particle_phis.append(p_phi)
-                    elif particle.pid == -target:
+                    elif particle.pid == -signal_p_pid:
                         num_particles += 1
                         n_pt = mev_to_gev( particle.momentum.pt() )
                         neg_particle_pts.append(n_pt)
@@ -62,7 +68,7 @@ with pyhepmc.open(hepmc_file) as f:
                         neg_particle_phis.append(n_phi)
 
 # HISTOGRAM STUFF
-tags = ['Positively charged muons', 'Negatively charged muons']
+tags = [f'Positively charged {signal_p}s', f'Negatively charged {signal_p}s']
 
 # --- pT ---
 pTs = [pos_particle_pts, neg_particle_pts]
@@ -72,13 +78,13 @@ pT_num_bins = np.linspace(0, 3000, 50)
 plt.hist(pTs, bins=pT_num_bins, histtype='bar', label=tags, stacked=True, edgecolor='black')
 
 plt.xlabel('pT [GeV]')
-plt.ylabel('Number of Muons')
-plt.title('Muon pT')
+plt.ylabel(f'number of {signal_p}s')
+plt.title(f'{signal_p} pT')
 
 plt.legend()
 plt.grid(color='c')
 
-plt.savefig('muon_pT.png', bbox_inches='tight')
+plt.savefig(f'{signal_p}_pT.png', bbox_inches='tight')
 plt.clf()
 
 # --- eta ---
@@ -89,13 +95,13 @@ eta_num_bins = np.linspace( -5, 5 )
 plt.hist(etas, bins=eta_num_bins, histtype='bar', label=tags, stacked=True, edgecolor='black')
 
 plt.xlabel('$\eta$')
-plt.ylabel('Number of Muons')
-plt.title('Muon $\eta$')
+plt.ylabel(f'number of {signal_p}s')
+plt.title(f'{signal_p} $\eta$')
 
 plt.legend()
 plt.grid(color='c')
 
-plt.savefig('muon_eta.png', bbox_inches='tight')
+plt.savefig(f'{signal_p}_eta.png', bbox_inches='tight')
 plt.clf()
 
 # --- phi ---
@@ -106,16 +112,16 @@ phi_num_bins = np.linspace( -np.pi, np.pi, 32 )
 plt.hist(phis, bins=phi_num_bins, histtype='bar', label=tags, stacked=True, edgecolor='black')
 
 plt.xlabel('$\phi$')
-plt.ylabel('Number of Muons')
-plt.title('Muon $\phi$')
+plt.ylabel(f'number of {signal_p}s')
+plt.title(f'{signal_p} $\phi$')
 
 plt.legend()
 plt.grid(color='c')
 
-plt.savefig('muon_phi.png', bbox_inches='tight')
+plt.savefig(f'{signal_p}_phi.png', bbox_inches='tight')
 
 # --- misc ---
-print(num_particles,'muons generated')
+print(num_particles,f'{signal_p}s generated')
 print(np.max(pTs),'max pT')
 print(eta_extrema,'eta extremum')
 print(np.sum(phis) / num_particles,'average phi')
